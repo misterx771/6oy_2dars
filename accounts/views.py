cd463cd6f66caa235a93c0d4d6949973414751eb
mister_x771 <akramjonakbaraliev2010@gmail.com>
2025-10-18 17:50:15 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def login_view(request):
-    
-    if request.POST:
+    if request.method == "POST":
         phone = request.POST.get("phone")
         password = request.POST.get("password")
-        user = authenticate(phone=phone, password=password)
-        if user:
+
+        user = authenticate(request, phone=phone, password=password)
+
+        if user is not None:
             login(request, user)
-    
+            messages.success(request, "Tizimga muvaffaqiyatli kirdingiz!")
+            return redirect("/")
+        else:
+            messages.error(request, "❌ Telefon yoki parol noto'g'ri!")
+            return render(request, "accounts/auth.html")
+
     return render(request, "accounts/auth.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("auth:login")
